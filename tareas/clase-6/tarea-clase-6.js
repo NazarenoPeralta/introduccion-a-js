a986febc9d0988ddfa6eb83e9018d2b1367908e1
Naza Peralta <nazaperalta@outlook.com>
2021-02-27 18:05:30 -0300

    Ejercicios clase 6 incompletos

--- tareas/clase-6/tarea-clase-6.js
+++ tareas/clase-6/tarea-clase-6.js
@@ -5,12 +5,30 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 */
+let $button = document.querySelector("button");
 
-
+function CreateInput(indice){
+    textoEdad = document.createElement('label');
+    textoEdad.appendChild('#integrantes');
+    textoEdad.value = "Ingrese edad del familiar N° "+ i++;
+    ingreseEdad = document.createElement('input');
+    ingreseEdad.appendChild('#integrantes');
+}
+ 
+$button.onclick = function Mostrar(){ 
+    
+    let $cantidadDePersonas = Number(document.querySelector("#cantidadDePersonas").value)
+    
+    for(let i = 0; i<Number($cantidadDePersonas);i++){
+        let indice = i
+        CreateInput(indice);
+        return false
+    }   
+}
 /*
 TAREA:
 Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
 Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
 
 Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
-*/
+*/ 
